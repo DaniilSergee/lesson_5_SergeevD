a928ecf90c2d471f4e727c870b9b93de75766987
DaniilSergee <d0rkm4nd@gmail.com>
2023-06-17 16:13:35 +0600

    WORK2 high level was fixed

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,27 @@ public class Main {
         System.out.println("health: " + krocozila.getHealth());
         System.out.println("damage: " + krocozila.getDamage());
         System.out.println("superAbility: " + krocozila.getBossDeffence());
-
-        for (int i = 0; i < createHeroes().length; i++) {
-            System.out.println("Hero"+ i + " Information: ");
-            System.out.println("health: " + createHeroes()[i].getHealth());
-            System.out.println("damage: " + createHeroes()[i].getDamage());
-            System.out.println("superAbility: " + createHeroes()[i].getSuperAbility());
+        System.out.println("----------");
+        Hero[] oldSchoolTeam = createHeroes();
+        for (int i = 0; i < oldSchoolTeam.length; i++) {
+            System.out.println("Hero №"+ i + " Information: ");
+            System.out.println("health: " + oldSchoolTeam[i].getHealth());
+            System.out.println("damage: " + oldSchoolTeam[i].getDamage());
+            if (oldSchoolTeam[i].getSuperAbility() == null){
+                System.out.println("superAbility: not_ability");
+            }else{
+                System.out.println("superAbility: " + oldSchoolTeam[i].getSuperAbility());
+            }
         }
     }
     public static Hero[] createHeroes(){
         Hero firefighter = new Hero(200, 300);
         Hero jungleman = new Hero(350, 120);
         Hero hunterDundee = new Hero(400,200, "Big Snare");
-        Hero[] oldSchoolTeam = new Hero[3];
-        oldSchoolTeam[0] = firefighter;
-        oldSchoolTeam[1] = jungleman;
-        oldSchoolTeam[2] = hunterDundee;
-        return oldSchoolTeam;
+        Hero[] Team = new Hero[3];
+        Team[0] = firefighter;
+        Team[1] = jungleman;
+        Team[2] = hunterDundee;
+        return Team;
     }
 }
